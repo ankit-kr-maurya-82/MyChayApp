221e4623e3a6d36dda73fa8196bf888bd9ad7124
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-11-17 22:14:40 +0530

    App.jsx add features

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user') || 'null'))
+  return token ? <Chat token={token} user={user} onLogout={() => {
+    localStorage.clear();
+    setToken(null);
+    setUser(null);
+  }}/>
+  : <Login onAuth={(t,u) => {
+    localStorage.setItem('token', t);
+    localStorage.setItem('user', JSON.stringify(u));
+    setToken(t);
+    setUser(u);
+  }}/>
 
-  return (
-    <>
-     
-    </>
-  )
+ 
 }
 
 export default App
